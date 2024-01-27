5a7222fe1958da86845bd667e4b75614b6c5c084
starostin13 <starostin13@outlook.com>
2024-01-28 00:29:40 +0300

    wip keyboards

--- CareBot/CareBot/handlers.py
+++ CareBot/CareBot/handlers.py
@@ -1,23 +1,41 @@
-from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+from telegram import InlineKeyboardMarkup, KeyboardButton, ReplyKeyboardMarkup, Update, InlineKeyboardButton
+from telegram.ext import ApplicationBuilder, CallbackQueryHandler, CommandHandler, ContextTypes
 
 import config
 import players_helper
 
 async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    await update.message.reply_text(f'Hello {update.effective_user.first_name}')
+    rules = [[InlineKeyboardButton("Kill Team", callback_data="killteam"), InlineKeyboardButton("Boarding Action",callback_data="boardingaction")], [InlineKeyboardButton("WH40k 10",callback_data="wh40k"), InlineKeyboardButton("Combat patrol",callback_data="combatpatrol")], [InlineKeyboardButton("Battlefleet",callback_data="battlefleet")]]
+    menu = InlineKeyboardMarkup(rules)
+    await update.message.reply_text(f'Choose the rules {update.effective_user.first_name}', reply_markup=menu)
     
 async def im_in(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     opponents = players_helper.get_opponents(update.effective_user)
     for opponent in opponents:
         reply_text += opponent
         players_helper.notify(opponent)
-    await update.message.reply_text(f'You faced with {reply_text}')
+    opponnentsContacts = [[KeyboardButton("Kill Team",request_contact="")]]
+    menu = ReplyKeyboardMarkup(opponnentsContacts)
+    await update.message.reply_text(f'You faced with {reply_text}', reply_markup=menu)
+    
+async def test(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_text(f'You faced')
 
+async def button(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Parses the CallbackQuery and updates the message text."""
+    query = update.callback_query
 
-app = ApplicationBuilder().token(config.crusade_care_bot_telegram_token).build()
+    # CallbackQueries need to be answered, even if no notification to the user is needed
+    # Some clients may have trouble otherwise. See https://core.telegram.org/bots/api#callbackquery
+    await query.answer()
 
-app.add_handler(CommandHandler("hello", hello))
-app.add_handler(CommandHandler("hello", im_in))
+    await query.edit_message_text(text=f"Selected option: {query.data}")
 
-app.run_polling()
+bot = ApplicationBuilder().token(config.crusade_care_bot_telegram_token).build()
+
+bot.add_handler(CommandHandler("start", hello))
+bot.add_handler(CommandHandler("imin", im_in))
+bot.add_handler(CommandHandler("Battlefleet", test))
+bot.add_handler(CallbackQueryHandler(button))
+
+bot.run_polling()
