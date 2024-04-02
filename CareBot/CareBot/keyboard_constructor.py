ffe5d05624c714880ae7d02351ee9b0f7d25ba6b
AlekseyGerasimov <163829358+AlekseyGerasimov@users.noreply.github.com>
2024-04-02 22:57:50 +0300

    wip

--- CareBot/CareBot/keyboard_constructor.py
+++ CareBot/CareBot/keyboard_constructor.py
@@ -1,16 +1,20 @@
-from telegram import InlineKeyboardButton
+﻿from telegram import InlineKeyboardButton
 import numpy as np
 
 import sqllite_helper
 
 rulesNames = ["Kill Team", "Boarding Action", "WH40k", "Combat Patrol", "Battlefleet"]
 
-async def get_keyboard_rules_keyboard_for_user(user_telegram: str):
-    allready_scheduled_items = sqllite_helper.get_schedule_by_user(user_telegram)
+async def get_keyboard_rules_keyboard_for_user(user_telegram: str):        
+    buttons = list(map(lambda ruleName: InlineKeyboardButton(text=formatForButton(ruleName, user_telegram), callback_data=ruleName.replace(' ', '_').lover()), rulesNames))
         
-    buttons = list(map(lambda ruleName: InlineKeyboardButton(text=ruleName, callback_data=ruleName.replace(' ', '_').lover()), rulesNames))
-    
     rules = np.reshape(buttons, int(len(buttons) ** 0.5))
     
     return rules
-    
+ 
+def formatForButton(text, user_telegram):
+    allready_scheduled_items = sqllite_helper.get_schedule_by_user(user_telegram)
+    if text in allready_scheduled_items:
+        return '✅ ' + text
+    else:
+        return '➕ ' + text
