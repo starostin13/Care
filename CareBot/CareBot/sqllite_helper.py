3ae82217f83fe95b069aaf8170b3eb6fa0f2b9e9
AlekseyGerasimov <163829358+AlekseyGerasimov@users.noreply.github.com>
2024-04-02 22:57:50 +0300

    wip

--- CareBot/CareBot/sqllite_helper.py
+++ CareBot/CareBot/sqllite_helper.py
@@ -1,7 +1,7 @@
-import sqlite3
+﻿import sqlite3
 from xmlrpc.client import DateTime
 
-conn = sqlite3.connect('db/database.db', check_same_thread=False)
+conn = sqlite3.connect(r"C:\Users\al-gerasimov\source\repos\Care\CareBot\CareBot\db\database", check_same_thread=False)
 cursor = conn.cursor()
 
 def insert_to_schedule(date: DateTime, rules: str, user_telegram: str):
@@ -11,3 +11,4 @@ def insert_to_schedule(date: DateTime, rules: str, user_telegram: str):
 def get_schedule_by_user(user_telegram: str):
 	result = cursor.execute('SELECT * FROM schedule WHERE user_telegram={}'.format(user_telegram))
 	return result.fetchall
+
