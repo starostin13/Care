4281a2abce848a658f7c85ef18c321b21b8cfa29
AlekseyGerasimov <163829358+AlekseyGerasimov@users.noreply.github.com>
2024-04-03 23:15:02 +0300

    Inserting to database WORKED!

--- CareBot/CareBot/handlers.py
+++ CareBot/CareBot/handlers.py
@@ -1,12 +1,15 @@
+from asyncio.windows_events import NULL
 from telegram import InlineKeyboardMarkup, KeyboardButton, ReplyKeyboardMarkup, Update, InlineKeyboardButton
 from telegram.ext import ApplicationBuilder, CallbackQueryHandler, CommandHandler, ContextTypes
 
 import config
 import players_helper
 import keyboard_constructor
+import sqllite_helper
 
 async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    rules = keyboard_constructor.get_keyboard_rules_keyboard_for_user(update.effective_user.id)
+    userId = update.effective_user.id
+    rules = await keyboard_constructor.get_keyboard_rules_keyboard_for_user(userId)
     menu = InlineKeyboardMarkup(rules)
     await update.message.reply_text(f'Choose the rules {update.effective_user.first_name}', reply_markup=menu)
     
@@ -30,6 +33,7 @@ async def button(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     # Some clients may have trouble otherwise. See https://core.telegram.org/bots/api#callbackquery
     await query.answer()
 
+    await sqllite_helper.insert_to_schedule(NULL, query.data, update.effective_user.id)
     await query.edit_message_text(text=f"Selected option: {query.data}")
 
 bot = ApplicationBuilder().token(config.crusade_care_bot_telegram_token).build()
