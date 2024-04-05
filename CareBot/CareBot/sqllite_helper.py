bd58878e56f14188809533ac209ceeffa3488df4
AlekseyGerasimov <163829358+AlekseyGerasimov@users.noreply.github.com>
2024-04-06 00:15:56 +0300

    add dates keyboard

--- CareBot/CareBot/sqllite_helper.py
+++ CareBot/CareBot/sqllite_helper.py
@@ -1,3 +1,4 @@
+import datetime
 import sqlite3
 from xmlrpc.client import DateTime
 
@@ -5,7 +6,10 @@ conn = sqlite3.connect(r"C:\Users\al-gerasimov\source\repos\Care\CareBot\CareBot
 cursor = conn.cursor()
 
 async def insert_to_schedule(date: DateTime, rules: str, user_telegram: str):
-	cursor.execute('INSERT INTO schedule (date, rules, user_telegram) VALUES (?, ?, ?)', (str(date), rules, user_telegram))
+	weekNumber = 0
+	#result = cursor.execute('DELETE FROM schedule WHERE date_week<>?', weekNumber)
+	
+	cursor.execute('INSERT INTO schedule (date, rules, user_telegram, date_week) VALUES (?, ?, ?, ?)', (str(date), rules, user_telegram, weekNumber))
 	conn.commit()
 	
 def get_schedule_by_user(user_telegram: str):
