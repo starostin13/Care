7149be0e761f42f51c15f6149b92fe37e28bf66f
AlekseyGerasimov <163829358+AlekseyGerasimov@users.noreply.github.com>
2024-04-08 18:48:36 +0300

    different callbacks trigered ok

--- CareBot/CareBot/keyboard_constructor.py
+++ CareBot/CareBot/keyboard_constructor.py
@@ -5,12 +5,17 @@ import sqllite_helper
 
 async def get_keyboard_rules_keyboard_for_user(user_telegram: str):
     allready_scheduled_items = sqllite_helper.get_schedule_by_user(user_telegram)
-    rulesNames = ["Kill Team", "Boarding Action", "WH40k", "Combat Patrol", "Battlefleet"]
     
     #for ruleName in rules:
     #    data = ruleName.relace(' ', '_').lower()
     
-    rules = [[InlineKeyboardButton("Kill Team", callback_data="killteam"), InlineKeyboardButton("Boarding Action",callback_data="boardingaction")], [InlineKeyboardButton("WH40k 10",callback_data="wh40k"), InlineKeyboardButton("Combat patrol",callback_data="combatpatrol")], [InlineKeyboardButton("Battlefleet",callback_data="battlefleet")]]
+    rules = [
+        [InlineKeyboardButton("Kill Team", callback_data="rule:killteam")],
+        [InlineKeyboardButton("Boarding Action",callback_data="rule:boardingaction")],
+        [InlineKeyboardButton("WH40k 10",callback_data="rule:wh40k")],
+        [InlineKeyboardButton("Combat patrol",callback_data="rule:combatpatrol")],
+        [InlineKeyboardButton("Battlefleet",callback_data="rule:battlefleet")],
+    ]
     return rules
     
 async def this_week():
