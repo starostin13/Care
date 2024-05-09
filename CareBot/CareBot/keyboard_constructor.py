7335ce4701743a95cbbb8a5bbe33eeec8fcecfbe
AlekseyGerasimov <163829358+AlekseyGerasimov@users.noreply.github.com>
2024-05-09 23:03:14 +0300

    wip

--- CareBot/CareBot/keyboard_constructor.py
+++ CareBot/CareBot/keyboard_constructor.py
@@ -70,7 +70,7 @@ async def this_week(rule):
 
 async def today_schedule(user_id):
     today = dt.today()
-    appointments = sqllite_helper.get_schedule_by_user(user_id, today.strftime("%c"))
-    buttons = map(lambda ap: InlineKeyboardButton(ap),appointments)
+    appointments = sqllite_helper.get_schedule_by_user(user_id, str(today.date()))
+    buttons = map(lambda ap: InlineKeyboardButton(f'{ap[2]} {ap[3]}', f'mission_sch_{ap[0]}'),appointments)
     
-    return [[buttons]]
+    return [[list(buttons)]]
